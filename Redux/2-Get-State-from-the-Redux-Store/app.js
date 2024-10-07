e0368c584d0e619a4c8856d53993f17c874fdba7
ChAl76 <chornyalexander@gmail.com>
2024-10-07 19:16:31 +0300

    1th task completed

---
+++ Redux/2-Get-State-from-the-Redux-Store/app.js
@@ -0,0 +1,6 @@
+// The code from the previous challenge is re-written more concisely in the code editor.
+// Use store.getState() to retrieve the state from the store, and assign this to a new variable currentState.
+// SOLUTION:
+const store = Redux.createStore((state = 5) => state);
+
+// Change code below this line

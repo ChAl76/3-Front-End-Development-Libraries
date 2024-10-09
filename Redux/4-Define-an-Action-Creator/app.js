c2e2410222a152fbb970c58e6514c3d8e7852de4
ChAl76 <chornyalexander@gmail.com>
2024-10-09 13:30:58 +0300

    3-6 tasks completed

---
+++ Redux/4-Define-an-Action-Creator/app.js
@@ -0,0 +1,10 @@
+// Define a function named actionCreator() that returns the action object when called.
+// SOLUTION:
+// Define an action here:
+const action = {
+  type: 'LOGIN',
+};
+// Define an action creator here:
+function actionCreator() {
+  return action;
+}
